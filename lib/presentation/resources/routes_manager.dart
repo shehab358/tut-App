3c2b6e1c42a5554241f676f6324921cead5efcba
shehab358 <shehabyusuf12@gmail.com>
2024-06-30 17:25:18 +0300

    Finished Route MAnager

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/details/details_screen.dart';
+import 'package:tut_app/presentation/forgetpassword/forget_password_screen.dart';
+import 'package:tut_app/presentation/login/login_screen.dart';
+import 'package:tut_app/presentation/main/main_screen.dart';
+import 'package:tut_app/presentation/register/register_screen.dart';
+import 'package:tut_app/presentation/resources/strings_manager.dart';
+import 'package:tut_app/presentation/splash/splash_screen.dart';
+
 class Routes {
   static const String splashRoute = "/";
   static const String loginRoute = "/login";
@@ -5,5 +14,48 @@ class Routes {
   static const String forgetPasswordRoute = "/forgetPassword";
   static const String mainRoute = "/main";
   static const String storeDetailsRoute = "/storeDetails";
+}
 
+class RoutesGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterScreen());
+
+      case Routes.forgetPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgetPasswordScreen());
+
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainScreen());
+
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const DetailsScreen());
+
+      default:
+        return unDefinedRoute();
+    }
+  }
+
+  static Route<dynamic> unDefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            StringsManager.noRouteFound,
+          ),
+        ),
+        body: const Center(
+          child: Text(
+            StringsManager.noRouteFound,
+          ),
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/presentation/resources/strings_manager.dart
@@ -0,0 +1,3 @@
+class StringsManager {
+  static const noRouteFound = "No Route Found";
+}
